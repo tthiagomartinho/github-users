f67b6c64ce69a61583430a4194ec6b0a469621c6
Thiago Martinho <Thiago Martinho>
2016-07-28 22:04:19 -0300

    Including constants file

---
+++ app/src/main/java/br/com/martinho/githubusers/util/Constants.java
@@ -0,0 +1,9 @@
+package br.com.martinho.githubusers.util;
+
+/**
+ * Created by Thiago on 28/07/2016.
+ */
+public interface Constants {
+    String LINK_HEADER = "Link";
+    String LAST_ELEMENT = "last";
+}
